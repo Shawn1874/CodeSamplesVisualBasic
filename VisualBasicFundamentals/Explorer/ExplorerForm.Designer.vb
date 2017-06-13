dfa56241e99c7d2cd2c1a919593eabe0d1f88da4
Shawn1874 <kempofighter@gmail.com>
2017-06-12 19:12:52 -0700

    sdf: added explorer project

---
+++ VisualBasicFundamentals/Explorer/ExplorerForm.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ExplorerForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.FileTree = New System.Windows.Forms.TreeView()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.LargeImageList = New System.Windows.Forms.ImageList(Me.components)
+        Me.SmallImageList = New System.Windows.Forms.ImageList(Me.components)
+        Me.ViewLabel = New System.Windows.Forms.ToolStripLabel()
+        Me.ViewComboBox = New System.Windows.Forms.ToolStripComboBox()
+        Me.ToolStrip1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ViewLabel, Me.ViewComboBox})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(284, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 25)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.FileTree)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.ListView1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(284, 237)
+        Me.SplitContainer1.SplitterDistance = 94
+        Me.SplitContainer1.TabIndex = 1
+        '
+        'FileTree
+        '
+        Me.FileTree.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FileTree.Location = New System.Drawing.Point(0, 0)
+        Me.FileTree.Name = "FileTree"
+        Me.FileTree.Size = New System.Drawing.Size(94, 237)
+        Me.FileTree.TabIndex = 0
+        '
+        'ListView1
+        '
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView1.LargeImageList = Me.LargeImageList
+        Me.ListView1.Location = New System.Drawing.Point(0, 0)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(186, 237)
+        Me.ListView1.SmallImageList = Me.SmallImageList
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'LargeImageList
+        '
+        Me.LargeImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit
+        Me.LargeImageList.ImageSize = New System.Drawing.Size(32, 32)
+        Me.LargeImageList.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'SmallImageList
+        '
+        Me.SmallImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit
+        Me.SmallImageList.ImageSize = New System.Drawing.Size(32, 32)
+        Me.SmallImageList.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'ViewLabel
+        '
+        Me.ViewLabel.Name = "ViewLabel"
+        Me.ViewLabel.Size = New System.Drawing.Size(32, 22)
+        Me.ViewLabel.Text = "View"
+        '
+        'ViewComboBox
+        '
+        Me.ViewComboBox.Items.AddRange(New Object() {"Large Icons", "Small Icons", "Details"})
+        Me.ViewComboBox.Name = "ViewComboBox"
+        Me.ViewComboBox.Size = New System.Drawing.Size(121, 25)
+        '
+        'ExplorerForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "ExplorerForm"
+        Me.Text = "ExplorerStyleApplication"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents FileTree As System.Windows.Forms.TreeView
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents LargeImageList As System.Windows.Forms.ImageList
+    Friend WithEvents SmallImageList As System.Windows.Forms.ImageList
+    Friend WithEvents ViewLabel As System.Windows.Forms.ToolStripLabel
+    Friend WithEvents ViewComboBox As System.Windows.Forms.ToolStripComboBox
+
+End Class
---
+++ VisualBasicFundamentals/Explorer/ExplorerForm.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO
+
+Public Class ExplorerForm
+
+    Private Sub ExplorerForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ViewComboBox.SelectedIndex = 0
+        Dim docs As New TreeNode("C5 Docs")
+        docs.Tag = "E:\C5 Docs"
+        FileTree.Nodes.Add(docs)
+        GetFolders(docs)
+
+        docs.Expand() '
+
+        ListView1.Columns.Add("Name", 250)
+        ListView1.Columns.Add("Date modified", 150)
+        ListView1.Columns.Add("Size", 75, HorizontalAlignment.Right)
+    End Sub
+
+    Private Sub GetFolders(node As TreeNode)
+        Try
+            Dim dir As New DirectoryInfo(node.Tag)
+            For Each childDir In dir.GetDirectories()
+                If childDir.Attributes.HasFlag(FileAttributes.Hidden) Then
+                    Continue For
+                End If
+                Dim childNode As New TreeNode(childDir.Name)
+                childNode.Tag = childDir.FullName
+                node.Nodes.Add(childNode)
+                GetFolders(childNode)
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub FileTree_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles FileTree.AfterSelect
+        Dim dir As New DirectoryInfo(e.Node.Tag)
+        ListView1.Items.Clear()
+        LargeImageList.Images.RemoveByKey(".exe")
+        For Each thisFile In dir.GetFiles()
+            Dim item As New ListViewItem(thisFile.Name)
+            Dim lastWrite = thisFile.LastWriteTime
+            item.SubItems.Add(lastWrite.ToShortDateString() & " " & lastWrite.ToShortTimeString())
+            item.SubItems.Add(Math.Ceiling(thisFile.Length / 1024) & " KB")
+            If Not LargeImageList.Images.ContainsKey(thisFile.Extension) Then
+                Dim thisIcon = Icon.ExtractAssociatedIcon(thisFile.FullName)
+                LargeImageList.Images.Add(thisFile.Extension, thisIcon)
+                SmallImageList.Images.Add(thisFile.Extension, thisIcon)
+            End If
+            item.ImageKey = thisFile.Extension
+            ListView1.Items.Add(item)
+
+        Next
+    End Sub
+
+    Private Sub ToolStripLabel1_Click(sender As Object, e As EventArgs) Handles ViewLabel.Click
+
+    End Sub
+
+    Private Sub ViewComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ViewComboBox.SelectedIndexChanged
+        Select Case ViewComboBox.Text
+            Case "Large Icons"
+                ListView1.View = View.LargeIcon
+            Case "Small Icons"
+                ListView1.View = View.SmallIcon
+            Case "Details"
+                ListView1.View = View.Details
+        End Select
+    End Sub
+End Class
